905371dc63c5ee88f4d0d45e894a764ee16fe6ac
xiaoyang <ky6150450@gmail.com>
2020-02-27 18:15:46 +0800

    临时提交

--- SimpleLexer.java
+++ SimpleLexer.java
@@ -1,3 +1,7 @@
+import java.io.CharArrayReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *简单的词法分析器
@@ -5,19 +9,101 @@
  */
 public class SimpleLexer {
     public static void main(String args[]) {
-//        SimpleLexer lexer = new SimpleLexer();
-
+        SimpleLexer lexer = new SimpleLexer();
+       var tokens = new ArrayList<Token>();
+        System.out.println(tokens);
         String script = "int age = 26;";
         System.out.println("parse :" + script);
+        SimpleTokenReader tokenReader = lexer.tokenize(script);
     }
+    private StringBuffer tokenText = null;
+    private SimpleToken token = null;
+    private List<Token> tokens = null;
     private boolean isAlpha(int ch) {return ch >= 'a' && ch <='z' || ch >= 'A' && ch <='Z';}
     private DfaState initToken(char ch) {
         DfaState newState = DfaState.Initial;
         if (isAlpha(ch)) {
-            token.type
+//            token.type
+        }
+        return newState;
+    }
+    public SimpleTokenReader tokenize(String code) {
+        tokens = new ArrayList<Token>();
+        CharArrayReader reader = new CharArrayReader(code.toCharArray());
+        tokenText = new StringBuffer();
+        token = new SimpleToken();
+        int ich = 0;
+        char ch = 0;
+        DfaState state = DfaState.Initial;
+        try {
+            while ((ich = reader.read()) != -1) {
+                ch = (char) ich;
+                switch (state) {
+                    case Initial:
+                        state = initToken(ch);
+                        break;
+                }
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new SimpleTokenReader(tokens);
+    }
+    private class SimpleTokenReader implements TokenReader {
+       List<Token> tokens = null;
+       int pos = 0;
+
+       public SimpleTokenReader(List<Token> tokens) {this.tokens = tokens;}
+
+        @Override
+        public Token read() {
+           if (pos < tokens.size())  {
+               System.out.println(tokens);
+               return tokens.get(pos++);
+
+           }
+            return null;
+        }
+
+        @Override
+        public Token peek() {
+            if (pos < tokens.size()) {
+                return tokens.get(pos);
+            }
+            return null;
+        }
+
+        @Override
+        public void unread() {
+            if (pos > 0) {
+                pos--;
+            }
+        }
+
+        @Override
+        public int getPosition() {
+            return pos;
+        }
+
+        @Override
+        public void setPosition(int position) {
+            if (position >= 0 && position < tokens.size()) {
+                pos = position;
+            }
         }
     }
+    private final class SimpleToken implements Token {
+        // Token 类型
+        private TokenType type = null;
+        // 文本值
+        private String text = null;
 
+        @Override
+        public TokenType getType() { return type; }
+
+        @Override
+        public String getText() { return text; }
+    }
     private enum DfaState {
         Initial,
 
---
+++ TokenReader.java
@@ -0,0 +1,7 @@
+public interface TokenReader {
+    public Token read();
+    public Token peek();
+    public void unread();
+    public int getPosition();
+    public void setPosition(int position);
+}
